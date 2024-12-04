41254387135f79a101be3fb55eb20bc38af47b28
Tobi Kasaba <oluwatobikasaba12@gmail.com>
2024-12-04 09:44:31 +0000

    Add group admin inline and initial group URLs
    Introduce GroupMemberInline to enable inline editing of GroupMember instances directly on the Group admin page, enhancing the admin interface usability. Additionally, define initial URL patterns for listing, creating, and viewing individual groups, setting groundwork for group-related views.

--- simplesocial/groups/admin.py
+++ simplesocial/groups/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from . import models
+
 
 # Register your models here.
+
+class GroupMemberInline(admin.TabularInline):
+    """
+    admin.TabularInline is a subclass of admin.InlineModelAdmin in Django's admin interface
+
+    It is used to define inline editing for a model related to another model
+    through a foreign key or many-to-many relationship.
+
+    It allows you to edit related objects (e.g., GroupMember) directly within
+    the parent object's (e.g., Group) admin page in the admin site.
+    ------------------------------------------------------------------
+    It provides inline editing capabilities for the GroupMember model within
+    the Django admin interface using a tabular layout.
+
+    This class is used to facilitate the management of GroupMember instances
+    directly from the parent model's page in the admin site, allowing for a more
+    streamlined user experience when dealing with related data entries.
+
+    :ivar model: Specifies the model class that this inline administers.
+    :type model: models.GroupMember
+    """
+    model = models.GroupMember
+
+
+admin.site.register(models.Group)
