3fb321ca77a423043d02d91988709e52f09a5f22
Tobi Kasaba <oluwatobikasaba12@gmail.com>
2024-12-04 09:14:53 +0000

    Add group templates and views for group functionality
    This commit introduces templates for group creation, listing, and details, leveraging Django's generic views to manage groups. The `group_base.html` template provides a structural foundation, while `group_detail.html` and `group_form.html` offer specific content and form handling for group management. Additionally, a file extension correction from `.py` to `.html` for `group_list` is made to reflect the content accurately.

--- simplesocial/groups/views.py
+++ simplesocial/groups/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.urls import reverse
+from django.views import generic
+from .models import Group, GroupMember
+
 
 # Create your views here.
+class CreateGroup(LoginRequiredMixin, generic.CreateView):
+    fields = ('name', 'description')
+    model = Group
+    context_object_name = 'group'
+
+
+class SingleGroup(generic.DetailView):
+    model = Group
+
+
+class ListGroups(generic.ListView):
+    model = Group
